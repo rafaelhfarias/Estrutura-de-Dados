0674972e59d27209fcebc78452391ef0f7191e0f
Rafael <rafaelhfarias@gmail.com>
2017-02-21 11:12:02 -0100

    Stack Completo

--- Estruturas de Dados/stack-dinamico.cpp
+++ Estruturas de Dados/stack-dinamico.cpp
@@ -8,14 +8,35 @@ typedef struct Node{
 
 int tam;
 
+int menu(void){
+	int opt;
+
+	printf("Escolha a opcao\n");
+	printf("0. Sair\n");
+	printf("1. Zerar pilha\n");
+	printf("2. Exibir pilha\n");
+	printf("3. Push\n");
+	printf("4. Pop\n");
+	printf("Opcao: "); scanf("%d",&opt);
+	return opt;
+}
+
+
 void inicia(node *pilha){
-	pilha -> prox =NULL;
-	tam =0
+	pilha->prox =NULL;
+	tam =0;
+}
+
+int vazia(node *pilha){
+	if(pilha->prox == NULL)
+		return 1;
+	else
+		return 0;
 }
 
 node *aloca(){
 	node *novo = (node *)malloc(sizeof(node));
-	if (!node){
+	if (!novo){
 		printf("Nao ha memoria suficiente\n");
 		exit(1);
 	}else{
@@ -33,15 +54,17 @@ void push(node *pilha){
 	else{
 		node *tmp = pilha->prox;
 		while(tmp->prox != NULL)
-			tmp = tmp-prox;
+			tmp = tmp->prox;
 	tmp->prox = novo;
 	}
 	tam++;
 }
 
 node *pop(node *pilha){
-	if(pilha->prox == NULL)
+	if(pilha->prox == NULL){
 		printf("A pilha ja esta vazia\n");
+		return NULL;
+	}
 	else{
 		node *ultimo = pilha->prox, *penultimo = pilha;
 		while (ultimo->prox != NULL){
@@ -53,6 +76,69 @@ node *pop(node *pilha){
 		return ultimo;
 	}
 }
+
+void exibe(node *pilha){
+	if (vazia(pilha)){
+		printf("A pilha esta vazia.\n\n");
+		return ;
+	}
+	node *tmp;
+	tmp = pilha->prox;
+	printf("A pilha:\n");
+	while(tmp != NULL){
+		printf("%5d",tmp->n);
+		tmp = tmp->prox;
+	}
+	printf("\n\n");
+}
+
+
+void libera(node *pilha){
+	if (!vazia(pilha)){
+		node *proxNode, *atual;
+
+		atual = pilha->prox;
+		while (atual!= NULL){
+			proxNode = atual->prox;
+			free(atual);
+			atual = proxNode;
+		}
+	}
+}
+
+void opcao(node *PILHA, int op)
+{
+ node *tmp;
+ switch(op){
+  case 0:
+   libera(PILHA);
+   break;
+
+  case 1:
+   libera(PILHA);
+   inicia(PILHA);
+   break;
+
+  case 2:
+   exibe(PILHA);
+   break;
+
+  case 3:
+   push(PILHA);
+   break;
+
+  case 4:
+   tmp= pop(PILHA);
+   if(tmp != NULL)
+   printf("Retirado: %3d\n\n", tmp->n);
+   break;
+
+  default:
+   printf("Comando invalido\n\n");
+ }
+}
+
+
 
 int main(){
 	node *pilha = (node *)malloc(sizeof(node));
